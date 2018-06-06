ce7679da8195b4c2709746c2f07ef3bcbc968abb
Rafael Grigorian <me@raffi.io>
2018-06-05 22:48:27 -0500

    Uploading index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const path = require ("path")
+
+function deepestCommonFolder ( paths ) {
+	// Sanitize and normalize data
+	let data = paths
+		.map ( p => path.resolve ( p ) )
+		.map ( p => path.normalize ( p ) )
+		.map ( p => p.replace ( /^\/|$\//g, "" ) )
+		.map ( p => p.split ("/") )
+	// If empty array is passed, return root directory
+	if ( data.length == 0 ) return "/"
+	// Initialize traversal variables and helper functions
+	let same = ( i ) => !i.includes ( false ) && i.every ( j => j === i [ 0 ] )
+	let next = ( i ) => i.map ( j => j.length > 0 ? j.shift () : false )
+	let base = "/"
+	// Traverse through data and build base path
+	for ( let i = next ( data ); same ( i ); i = next ( data ) ) {
+		base += i [ 0 ] + "/"
+	}
+	// Return the normalized base directory
+	return path.normalize ( base )
+}
+
+module.exports = deepestCommonFolder
